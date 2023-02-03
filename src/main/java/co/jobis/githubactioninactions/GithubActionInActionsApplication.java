722dca8fffd09834513a4e50bbc37bfc41c91d35
eungi <egchoi@jobis.co>
2023-02-03 15:25:33 +0900

    init

---
+++ src/main/java/co/jobis/githubactioninactions/GithubActionInActionsApplication.java
@@ -0,0 +1,13 @@
+package co.jobis.githubactioninactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionInActionsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GithubActionInActionsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/co/jobis/githubactioninactions/GithubActionInActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.jobis.githubactioninactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionInActionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
